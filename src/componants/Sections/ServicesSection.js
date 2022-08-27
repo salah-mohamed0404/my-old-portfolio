02ffeb3b6991db0faf98a2a1e6a03a4ac0854c62
salahcst344 <salahcst344@gmail.com>
2022-08-27 21:02:43 +0200

    Build service section

--- src/componants/Sections/ServicesSection.js
+++ src/componants/Sections/ServicesSection.js
@@ -1,9 +1,31 @@
 import classes from "./ServicesSection.module.css";
+import serviceImg from "../../assets/MERN services.png";
 
 function Services() {
   return (
     <section className={classes["services-section"]}>
-      <h2>Services</h2>
+      <div className="container">
+        <h2 className="heading-secondary">Services</h2>
+        <div className={classes.main}>
+          <div>
+            <h3 className="heading-tertiary">MERN Stack</h3>
+            <h4 className="subheading">
+              (ReactJS, NodeJS, Express, and MongoDB)
+            </h4>
+            <p>
+              I know that good development means good business So I keen to make
+              consistent, efficient and powerful full-stack websites with fully
+              functional and user friendly user interface that will empower your
+              business.
+            </p>
+            <a href="#projects" className="link btn">
+              Projects
+            </a>
+          </div>
+
+          <img src={serviceImg} alt="MERN technologies logo" />
+        </div>
+      </div>
     </section>
   );
 }

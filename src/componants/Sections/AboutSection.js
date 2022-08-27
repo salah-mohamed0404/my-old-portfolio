a1f21be6e61178ab6ceb004c11c171eb958a373e
salahcst344 <salahcst344@gmail.com>
2022-08-27 19:05:38 +0200

    build about me

--- src/componants/Sections/AboutSection.js
+++ src/componants/Sections/AboutSection.js
@@ -1,23 +1,29 @@
-import classes from "./AboutSection.nodule.css";
+import classes from "./AboutSection.module.css";
+import aboutImg from "../../assets/me4.jpg";
 
 function AboutSection() {
   return (
     <section id="about" className={classes["about-section"]}>
-      <div className={classes["about-text"]}>
-        <h2>About me</h2>
-        <p>
-          Hello! My name is Salah Mohamed.
-          <br />
-          I'm a College student in computers and AI faculty, Helwan university,
-          and self learning web developer who is passionate about programming in
-          gerneral and web development in particular.
-        </p>
-        <a href="#edu&exp" className="btn" />
+      <div className="container">
+        <h2 className="heading-secondary">About me</h2>
+        <div className={classes.main}>
+          <div className={classes["about-text"]}>
+            <h3 className="heading-tertiary">Hello! My name is Salah.</h3>
+            <p>
+              <br />
+              I'm a College student in computers and AI faculty, Helwan
+              university, and self learning web developer who is passionate
+              about programming in gerneral and web development in particular.
+            </p>
+            <a href="#edu&exp" className="link">
+              Cetificates
+            </a>
+          </div>
+          <figure>
+            <img src={aboutImg} alt="for me" />
+          </figure>
+        </div>
       </div>
-
-      <figure>
-        <img src="" alt="" />
-      </figure>
     </section>
   );
 }

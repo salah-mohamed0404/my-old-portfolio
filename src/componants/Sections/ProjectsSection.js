5de2ab727eb8d79d20ead4e7e0a769863ffb0840
salahcst344 <salahcst344@gmail.com>
2022-08-30 00:29:05 +0200

    Build Projects sections

--- src/componants/Sections/ProjectsSection.js
+++ src/componants/Sections/ProjectsSection.js
@@ -1,35 +1,63 @@
+import { useState } from "react";
+import { projects } from "../../store/data";
 import classes from "./ProjectsSection.module.css";
 
+const btns = document.querySelectorAll(`.${classes["projects-section"]} .btn`);
+
 function Projects() {
-  const renderProjects = (project) => (
-    <li className={classes["project-item"]}>
-      <img src="" alt="" />
-      <h3>{project.name}</h3>
-      <p>{project.description}</p>
-      <span className={classes["project-tech"]}>{project.technolgy}</span>
+  let filteredProjects;
+  const listItems = document.querySelectorAll(
+    `.${classes["projects-section"]} li`
+  );
+  const [active, setActive] = useState("Featured");
+  const technolgies = [...new Set(projects.map(project => project.technology))];
+
+  // Filter project based on active button
+  if (active === "Featured")
+    filteredProjects = projects.filter(project => project.featured);
+  else
+    filteredProjects = projects.filter(
+      project => project.technology === active
+    );
+
+  const btnFilterHandler = e => {
+    btns.forEach(btn => btn.classList.remove("btn_active"));
+    e.target.classList.add("btn_active");
+    setActive(e.target.textContent);
+  };
+
+  const renderProject = project => (
+    <li key={project.name} className="project-item">
+      <figure>
+        <img src={project.img} alt={project.name} />
+      </figure>
+      <div>
+        <div>
+          <h3>{project.name}</h3>
+          <p>{project.description}</p>
+        </div>
+        <span>{project.technology}</span>
+      </div>
     </li>
   );
 
   return (
     <section id="projects" className={classes["projects-section"]}>
-      <h2>Projects</h2>
-      <nav className={classes["projects-nav"]}>
-        <button
-          className={`${classes["tech-filter-btn"]} tech-filter-btn__active`}
-        >
-          All
-        </button>
-        <button className={classes["tech-filter-btn"]}>React</button>
-        <button className={classes["tech-filter-btn"]}>JavaScript</button>
-        <button className={classes["tech-filter-btn"]}>HTML&CSS</button>
-        <button className={classes["tech-filter-btn"]}>NodeJS</button>
-      </nav>
+      <div className="container">
+        <h2 className="heading-secondary">Projects</h2>
+        <nav>
+          <button className="btn btn_active" onClick={btnFilterHandler}>
+            Featured
+          </button>
+          {technolgies.map(tech => (
+            <button className="btn" onClick={btnFilterHandler} key={tech}>
+              {tech}
+            </button>
+          ))}
+        </nav>
 
-      <ul className={classes["projects-list"]}>
-        {/* Render project dynamic */}
-      </ul>
-
-      {/* Pagnation here */}
+        <ul>{filteredProjects.map(project => renderProject(project))}</ul>
+      </div>
     </section>
   );
 }

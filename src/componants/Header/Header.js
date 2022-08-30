23fa1d89d4e3f91d813ac5c47a821e20cae889cb
salahcst344 <salahcst344@gmail.com>
2022-08-30 20:07:54 +0200

    Stiky Nav

--- src/componants/Header/Header.js
+++ src/componants/Header/Header.js
@@ -3,11 +3,40 @@ import HeroSection from "./HeroSection";
 
 import classes from "./Header.module.css";
 import logoImg from "../../assets/logo.png";
+import { useEffect, useState } from "react";
 
 function Header() {
+  const [first, refresh] = useState(false);
+  useEffect(() => refresh(true), []);
+  // Sticky navigation
+  if (first) {
+    const sectionHeroEl = document.getElementById("section-hero");
+
+    const obs = new IntersectionObserver(
+      function (entries) {
+        const ent = entries[0];
+
+        if (!ent.isIntersecting) {
+          document.body.classList.add("sticky");
+        }
+
+        if (ent.isIntersecting) {
+          document.body.classList.remove("sticky");
+        }
+      },
+      {
+        // In the viewport
+        root: null,
+        threshold: 0,
+        rootMargin: "-80px",
+      }
+    );
+    obs.observe(sectionHeroEl);
+  }
+
   return (
     <header className={classes["main-header"]}>
-      <header>
+      <header id="nav-header">
         <div className={classes.logo}>
           <img src={logoImg} alt="logo" />
         </div>

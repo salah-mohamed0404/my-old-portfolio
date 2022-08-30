f924dfbb68d494df9d37c532cc2395fc2f44c085
salahcst344 <salahcst344@gmail.com>
2022-08-30 20:07:34 +0200

    Implement revel sections

--- src/componants/Main.js
+++ src/componants/Main.js
@@ -4,8 +4,37 @@ import ServicesSection from "./Sections/ServicesSection";
 import SkillsAndCertficates from "./Sections/SkillsAndCertficatesSection";
 import ProjectsSection from "./Sections/ProjectsSection";
 import ContactUsSection from "./Sections/ContactUsSection";
+import { useEffect, useState } from "react";
 
 function Main() {
+  const [first, refresh] = useState(false);
+  useEffect(() => refresh(true), []);
+  // Reveal section
+  if (first) {
+    const allSections = document.querySelectorAll("main section");
+
+    const revealSection = function (entries, observer) {
+      const [entry] = entries;
+      console.log(entry);
+
+      if (!entry.isIntersecting) return;
+
+      entry.target.classList.remove("section--hidden");
+
+      observer.unobserve(entry.target);
+    };
+
+    const sectionObserver = new IntersectionObserver(revealSection, {
+      root: null,
+      threshold: 0.15,
+    });
+
+    allSections.forEach(section => {
+      sectionObserver.observe(section);
+      section.classList.add("section--hidden");
+    });
+  }
+
   return (
     <main className={classes.main}>
       <AboutSection />

9e1ceae1ff2343595ba25ae5b2444235a7bcb188
salahcst344 <salahcst344@gmail.com>
2022-08-31 22:14:43 +0200

    Parly fix a bug

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,24 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import Header from "./componants/Header/Header";
 import LoadingSpinner from "./componants/Loading/LoadingSpinner";
 import Main from "./componants/Main";
 
-let loaded = false;
-
 function Portfolio() {
-  const [isLoading, setIsLoading] = useState(true);
   const loadingEl = document.getElementById("loading");
 
-  window.addEventListener("load", () => loadingEl.classList.add("loaded"));
+  const [completed, setCompleted] = useState(false);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setCompleted(true);
+    }, 3000);
+  }, []);
+
+  if (completed) loadingEl.classList.add("loaded");
   return (
-    <>
+    <div>
       <Header />
       <Main />
-      {isLoading &&
-        createPortal(<LoadingSpinner />, document.getElementById("loading"))}
-    </>
+      {createPortal(<LoadingSpinner />, document.getElementById("loading"))}
+    </div>
   );
 }
 
